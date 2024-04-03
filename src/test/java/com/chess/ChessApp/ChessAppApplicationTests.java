e0b29a4257650914d087176ac43cf402019cf945
ctrumbo <camerontrumbo@gmail.com>
2024-04-02 22:11:16 -0400

    initial commit

---
+++ src/test/java/com/chess/ChessApp/ChessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess.ChessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
